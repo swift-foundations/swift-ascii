a07cdae794e5d6296806e5501323501fcd2afbf2
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-12 20:18:54 +0100

    Use swift-incits-4-1986-fork (has proper 0.0.1 tag with remote URLs)

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "ASCII", targets: ["ASCII"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-incits-4-1986.git", from: "0.0.1"),
+        .package(url: "https://github.com/swift-standards/swift-incits-4-1986-fork.git", from: "0.0.1"),
         .package(url: "https://github.com/swift-primitives/swift-binary-primitives.git", from: "0.0.1"),
         .package(url: "https://github.com/swift-primitives/swift-parsing-primitives.git", from: "0.0.1"),
         .package(url: "https://github.com/swift-primitives/swift-serialization-primitives.git", from: "0.0.1"),
@@ -27,7 +27,7 @@ let package = Package(
         .target(
             name: "ASCII",
             dependencies: [
-                .product(name: "INCITS 4 1986", package: "swift-incits-4-1986"),
+                .product(name: "INCITS 4 1986", package: "swift-incits-4-1986-fork"),
                 .product(name: "Binary Primitives", package: "swift-binary-primitives"),
                 .product(name: "Parsing Primitives", package: "swift-parsing-primitives"),
                 .product(name: "Serialization Primitives", package: "swift-serialization-primitives"),
